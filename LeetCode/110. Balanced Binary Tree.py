867ffe5ce04414ca8d23d7510feb176b48b0dca8
QinganZhao <qingan_zhao@berkeley.edu>
2018-12-24 22:50:29 -0500

    update sol

---
+++ LeetCode/110. Balanced Binary Tree.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        return self.check(root) != -1
+    
+    def check(self, root):
+        if not root:
+            return 0
+        left = self.check(root.left)
+        right = self.check(root.right)
+        if left == -1 or right == -1 or abs(left-right) > 1:
+            return -1
+        return 1 + max(left, right)
