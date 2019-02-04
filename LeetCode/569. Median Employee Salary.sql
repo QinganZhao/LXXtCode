1191ec160caa55f82c0debac84e265f75b19ac23
QinganZhao <qingan_zhao@berkeley.edu>
2019-02-04 10:47:33 -0500

    add sol

---
+++ LeetCode/569. Median Employee Salary.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT E1.id, E1.Company, E1.Salary
+FROM Employee E1, Employee E2
+WHERE E1.Company = E2.Company
+GROUP BY E1.Company, E1.Salary
+HAVING SUM(CASE WHEN E1.Salary = E2.salary THEN 1 ELSE 0 END) >= ABS(SUM(SIGN(E1.Salary - E2.Salary)))
+ORDER BY Company
