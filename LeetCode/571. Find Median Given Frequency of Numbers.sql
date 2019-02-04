2c476fc322c5d3f31be5daafb1174339b6190d51
QinganZhao <qingan_zhao@berkeley.edu>
2019-02-04 11:23:10 -0500

    add sol

---
+++ LeetCode/571. Find Median Given Frequency of Numbers.sql
@@ -0,0 +1,6 @@
+SELECT AVG(n.Number) median
+FROM Numbers n
+WHERE n.Frequency >= ABS((SELECT SUM(Frequency) FROM Numbers 
+                          WHERE Number <= n.Number) -
+                          (SELECT SUM(Frequency) FROM Numbers
+                           WHERE Number >= n.Number))
