058d1334ca1e2bd2d1c42bcc531ebf6ed5e416ac
QinganZhao <qingan_zhao@berkeley.edu>
2019-02-04 22:19:33 -0500

    minor change

--- 262. Trips and Users.sql
+++
@@ -1,7 +0,0 @@
-SELECT t.Request_at Day, 
-ROUND(SUM(CASE WHEN t.Status LIKE 'cancelled%' THEN 1 ELSE 0 END)/COUNT(*), 2) 'Cancellation Rate'
-FROM Trips t 
-JOIN Users u 
-ON t.Client_Id = u.Users_Id and u.Banned='No'
-WHERE t.Request_at between '2013-10-01' and '2013-10-03'
-GROUP BY t.Request_at
--- 626. Exchange Seats.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-(CASE WHEN MOD(id, 2) = 1 AND id != count_id THEN id + 1
-WHEN MOD(id, 2) = 1 AND id = count_id THEN id 
-ELSE id - 1 END) id, student
-FROM seat, 
-(SELECT COUNT(*) count_id FROM seat) counted_id
-ORDER BY id
