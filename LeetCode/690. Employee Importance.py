63f2237865392f2cd598ba57cd2d6ee0d8d7274f
QinganZhao <qingan_zhao@berkeley.edu>
2019-10-11 11:01:53 -0400

    update sol

--- LeetCode/690. Employee Importance.py
+++ LeetCode/690. Employee Importance.py
@@ -10,6 +10,9 @@ class Employee:
         # the id of direct subordinates
         self.subordinates = subordinates
 """
+
+# recursion
+'''
 class Solution:
     def getImportance(self, employees, id):
         """
@@ -22,3 +25,20 @@ class Solution:
     
     def dfs(self, dic, id):
         return dic[id].importance + sum([self.dfs(dic, i) for i in dic[id].subordinates])
+'''
+
+# iteration
+class Solution:
+    def getImportance(self, employees, id):
+        """
+        :type employees: Employee
+        :type id: int
+        :rtype: int
+        """
+        dic = {i.id: i for i in employees}
+        stack, res = [dic[id]], 0
+        while stack:
+            e = stack.pop()
+            res += e.importance
+            stack += [dic[i] for i in e.subordinates]
+        return res
